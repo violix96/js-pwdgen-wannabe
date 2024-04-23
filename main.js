1b8a65c51f78690787b1c5011f40bece7d57aa69
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-23 17:08:05 +0200

    [UPD] Added random number bonus

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let currentElement = document.getElementById('password');
 // riciclo il valore iniziale della variabile
 let currentText = currentElement.innerHTML;
 
+
 // chiedo i valori all'utente 
 
 const userName = prompt ('Inserisci il tuo nome')
@@ -16,8 +17,11 @@ const userLastName = prompt ('Inserisci il tuo cognome')
 
 const userFavColor = prompt ('Inserisci il tuo colore preferito')
 
+// bonus numero random 
+let randomNumber = Math.floor((Math.random() * 100) + 21);
+
 // Interpolazione e quindi cambio di valori di currentText 
-currentText = `${currentText}${userName}${userLastName}${userFavColor}21`;
+currentText = `${currentText}${userName}${userLastName}${userFavColor}${randomNumber}`;
 
 // scrivo in console 
 console.log(currentText);

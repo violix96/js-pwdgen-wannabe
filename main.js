8e3e386bec2c7caf6063d5a173c658cacae33e3f
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-23 16:27:53 +0200

    [UPD] Select elements from the DOM

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// reset js
 'use strict';
+
+
+// Salvo l'elemento paragrafo all'interno di una variabile
+let currentElement = document.getElementById('password');
+
+// riciclo il valore iniziale della variabile
+let currentText = currentElement.innerHTML;

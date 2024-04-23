e7417562c8e02fe62846c47f8a909406f1b5d1ae
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-23 16:40:15 +0200

    [UPD]added interpolation for password generation

--- main.js
+++ main.js
@@ -15,3 +15,6 @@ const userName = prompt ('Inserisci il tuo nome')
 const userLastName = prompt ('Inserisci il tuo cognome')
 
 const userFavColor = prompt ('Inserisci il tuo colore preferito')
+
+// Interpolazione e quindi cambio di valori di currentText 
+currentText = `${currentText}${userName}${userLastName}${userFavColor}21`;

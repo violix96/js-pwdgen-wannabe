bb5feab1b3ca1458bafcbd49aee5da4a547935c0
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-23 16:33:25 +0200

    [UPD] added request for values ​​from the user

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ let currentElement = document.getElementById('password');
 
 // riciclo il valore iniziale della variabile
 let currentText = currentElement.innerHTML;
+
+// chiedo i valori all'utente 
+
+const userName = prompt ('Inserisci il tuo nome')
+
+const userLastName = prompt ('Inserisci il tuo cognome')
+
+const userFavColor = prompt ('Inserisci il tuo colore preferito')
